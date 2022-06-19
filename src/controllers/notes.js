48a9065c10e31aee5bda1824585b56d5a7d61d51
amirtha-coder <amirtha.m.muthirulandi@gmail.com>
2022-06-19 19:26:45 +0100

    createNote added

--- src/controllers/notes.js
+++ src/controllers/notes.js
@@ -1,6 +1,39 @@
-const path = require("path");
-const getNotes = () => {};
-const deleteNotes = () => {};
-const createNote = () => {};
+// import packages
+const { v4: uuidv4 } = require("uuid");
+// const path = require("path");
 
-module.exports = { getNotes, deleteNotes, createNote };
+// import modules
+const { readDataFromFile, writeDataFromFile } = require("../utils");
+
+const getNotes = (req, res) => {
+  // get Notes from utils
+  const notes = readDataFromFile("notes");
+
+  // send notes as response
+  return res.json(notes);
+};
+const createNote = (req, res) => {
+  const { title, text } = req.body;
+  console.log("delete");
+
+  const id = uuidv4();
+
+  const note = {
+    title,
+    text,
+    id,
+  };
+
+  const data = readDataFromFile("notes");
+
+  data.notes.push(note);
+
+  writeDataFromFile("notes", data);
+
+  return res.json();
+};
+const deleteNotes = (req, res) => {
+  console.log("create");
+};
+
+module.exports = { getNotes, createNote, deleteNotes };
