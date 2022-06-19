8c95cf669c7a99894cdc577e80b123b39ad6e425
amirtha-coder <amirtha.m.muthirulandi@gmail.com>
2022-06-19 16:41:05 +0100

    read and write functions added

---
+++ src/utils.js
@@ -0,0 +1,22 @@
+// module imports
+const fs = require("fs");
+const path = require("path");
+
+const readDataFromFile = (fileName) => {
+  const filePath = path.join(__dirname, `./${fileName}.json`);
+
+  const data = fs.readFileSync(filePath, "utf8");
+
+  return JSON.parse(data);
+};
+
+const writeDataFromFile = (fileName, data) => {
+  const filePath = path.join(__dirname, `./${fileName}.json`);
+
+  fs.writeFileSync(filePath, JSON.stringify(data));
+};
+
+module.exports = {
+  readDataFromFile,
+  writeDataFromFile,
+};
